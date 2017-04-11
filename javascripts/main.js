3ef96f728014e8d2a24b35b7483afc9fd999a8b7
Latasha Simmons <lsimmons1832@gmail.com>
2017-04-10 19:54:32 -0500

    dino printing complete

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,23 @@
-console.log("I'm linked")
+var dinoArray = [];
+
+$.ajax('./db/dinosaurs.json').done(function(data){
+	dinoArray = data.dinosaurs;
+	makeDOM(dinoArray);
+}).fail(function(error){
+	console.log("Warning! There is an issue!", error);
+});
+
+function makeDOM(myArrayToPrint){
+	var myDOMString = '';
+	for (var i = 0; i < myArrayToPrint.length; i++) {
+		myDOMString += `<div class="dinoCard">`;
+		myDOMString += `<header><h1>${myArrayToPrint[i].type}</h1></header>`;
+		myDOMString += `<section>`;
+		myDOMString += `<img src="${myArrayToPrint[i].img}">`;
+		myDOMString += `<p class='bio'>${myArrayToPrint[i].bio}</p>`;
+		myDOMString += `</section>`;
+		myDOMString += `<footer>${myArrayToPrint[i].info}</footer>`;
+		myDOMString += `</div>`;
+	}
+	$('#dinosaur').append(myDOMString);
+}
